466611313cbcf19ae9fb37b14571ed6fc8ac5131
poautran <poautran@gmail.com>
2020-01-28 17:30:42 +0100

    Fix bugs

--- PyXRDCT/core/separate.py
+++ PyXRDCT/core/separate.py
@@ -21,6 +21,10 @@ def readJson(jsonFile):
 
 ### Input ###
 
+Threshold = 50
+
+### Main code ###
+
 def main():
 	parser = argparse.ArgumentParser(description='This script separates the powder from monocristalline part on 2D images. python separate.py FILE -j azimint.json')
 	parser.add_argument('INPUT',metavar="INPUT", help="List of files. Ex : *.edf", nargs='+')
@@ -52,26 +56,26 @@ def run(args):
 	mask = np.array(fabio.open(jsonParam['mask_file']).data)
 	for i in range(len(FILE)):
 		dataFile = np.array(fabio.open(FILE[i]).data)
-		dataBragg, dataPowder = azimutalIntegrator.separate(dataFile, npt_rad=1024, npt_azim=512, unit=jsonParam['unit'], method='splitpixel', percentile=50, mask=None, restore_mask=True)
-		dataXP, dataYP = azimutalIntegrator.integrate1d(dataPowder, int(jsonParam['nbpt_rad']), filename=None, correctSolidAngle=jsonParam['do_solid_angle'], variance=None, error_model=None, radial_range=(float(jsonParam['radial_range_min']),float(jsonParam['radial_range_max'])), azimuth_range=None, mask=mask, dummy=jsonParam['do_dummy'], delta_dummy=jsonParam['delta_dummy'], polarization_factor=None, method='csr', unit=jsonParam['unit'], safe=True, profile=False, all=False, metadata=None)
-		dataXB, dataYB = azimutalIntegrator.integrate1d(dataBragg, int(jsonParam['nbpt_rad']), filename=None, correctSolidAngle=jsonParam['do_solid_angle'], variance=None, error_model=None, radial_range=(float(jsonParam['radial_range_min']),float(jsonParam['radial_range_max'])), azimuth_range=None, mask=mask, dummy=jsonParam['do_dummy'], delta_dummy=jsonParam['delta_dummy'], polarization_factor=None, method='csr', unit=jsonParam['unit'], safe=True, profile=False, all=False, metadata=None)
-		dataXO, dataYO = azimutalIntegrator.integrate1d(dataFile, int(jsonParam['nbpt_rad']), filename=None, correctSolidAngle=jsonParam['do_solid_angle'], variance=None, error_model=None, radial_range=(float(jsonParam['radial_range_min']),float(jsonParam['radial_range_max'])), azimuth_range=None, mask=mask, dummy=jsonParam['do_dummy'], delta_dummy=jsonParam['delta_dummy'], polarization_factor=None, method='csr', unit=jsonParam['unit'], safe=True, profile=False, all=False, metadata=None)
+		dataBragg, dataPowder = azimutalIntegrator.separate(dataFile, npt_rad=1024, npt_azim=512, unit=jsonParam['unit'], method='splitpixel', percentile=Threshold, mask=None, restore_mask=True)
+		dataXP, dataYP = azimutalIntegrator.integrate1d(dataPowder, int(jsonParam['nbpt_rad']), filename=None, correctSolidAngle=jsonParam['do_solid_angle'], variance=None, error_model=None, radial_range=(float(jsonParam['radial_range_min']),float(jsonParam['radial_range_max'])), azimuth_range=None, mask=mask, dummy=jsonParam['do_dummy'], delta_dummy=jsonParam['delta_dummy'], polarization_factor=None, method=jsonParam['method'], unit=jsonParam['unit'], safe=True, profile=False, all=False, metadata=None)
+		dataXB, dataYB = azimutalIntegrator.integrate1d(dataBragg, int(jsonParam['nbpt_rad']), filename=None, correctSolidAngle=jsonParam['do_solid_angle'], variance=None, error_model=None, radial_range=(float(jsonParam['radial_range_min']),float(jsonParam['radial_range_max'])), azimuth_range=None, mask=mask, dummy=jsonParam['do_dummy'], delta_dummy=jsonParam['delta_dummy'], polarization_factor=None, method=jsonParam['method'], unit=jsonParam['unit'], safe=True, profile=False, all=False, metadata=None)
+		dataXO, dataYO = azimutalIntegrator.integrate1d(dataFile, int(jsonParam['nbpt_rad']), filename=None, correctSolidAngle=jsonParam['do_solid_angle'], variance=None, error_model=None, radial_range=(float(jsonParam['radial_range_min']),float(jsonParam['radial_range_max'])), azimuth_range=None, mask=mask, dummy=jsonParam['do_dummy'], delta_dummy=jsonParam['delta_dummy'], polarization_factor=None, method=jsonParam['method'], unit=jsonParam['unit'], safe=True, profile=False, all=False, metadata=None)
 		
 		# Display #
 		if args.DISPLAY:
 			ax1 = plt.subplot(2, 3, 1)
-			ax1.imshow(dataFile)
-			ax1.set_title('Original data')	
-			ax2 = plt.subplot(2, 3, 2)	
-			ax2.imshow(dataBragg)
+			ax1.imshow(dataFile,interpolation=None)
+			ax1.set_title('Full data')	
+			ax2 = plt.subplot(2, 3, 2,sharex=ax1,sharey=ax1)	
+			ax2.imshow(dataBragg,interpolation=None)
 			ax2.set_title('Bragg contribution')	
-			ax3 = plt.subplot(2, 3, 3)
-			ax3.imshow(dataPowder)
+			ax3 = plt.subplot(2, 3, 3,sharex=ax1,sharey=ax1)
+			ax3.imshow(dataPowder,interpolation=None)
 			ax3.set_title('Powder contribution')	
 			ax4 = plt.subplot(2, 1, 2)	
 			ax4.plot(dataXP, dataYP,label='Powder contribution')
 			ax4.plot(dataXB, dataYB,label='Bragg contribution')
-			ax4.plot(dataXO, dataYO,label='Original')
+			ax4.plot(dataXO, dataYO,label='Full')
 			ax4.set_title('Integrated data')
 			ax4.set_xlabel(r'$Q (\AA^{-1})$', fontsize=11)
 			ax4.set_ylabel(r'$I(A.U.)$', fontsize=11)
@@ -80,25 +84,22 @@ def run(args):
 
 		# Save Edf File #		
 
-		#f.open(FILE[:-4]+'powder_contrib.edf')
-
-
-
+		# Not ready #
 
 		# Save integrated data #
 		currentFILE = FILE[i]
-		f_out_dataPowder = open(currentFILE[:-4] + '_powder.dat','w')
+		f_out_dataPowder = open(currentFILE[:-4] + '_THR' + str(Threshold) + '_powder.dat','w')
 		for j in range(0,np.size(dataXP,0)):
 			f_out_dataPowder.writelines('%07f'%dataXP[j] + '    ' + '%04f'%dataYP[j]  + '\n')
 		f_out_dataPowder.close()
-		f_out_dataBragg = open(currentFILE[:-4] + '_bragg.dat','w')
+		f_out_dataBragg = open(currentFILE[:-4] + '_THR' + str(Threshold) + '_bragg.dat','w')
 		for j in range(0,np.size(dataXB,0)):
 			f_out_dataBragg.writelines('%07f'%dataXB[j] + '    ' + '%04f'%dataYB[j]  + '\n')
 		f_out_dataBragg.close()
-		f_out_dataOriginal = open(currentFILE[:-4] + '_original.dat','w')
+		f_out_dataFull = open(currentFILE[:-4] + '_THR' + str(Threshold) + '_full.dat','w')
 		for j in range(0,np.size(dataXO,0)):
-			f_out_dataOriginal.writelines('%07f'%dataXO[j] + '    ' + '%04f'%dataYO[j]  + '\n')
-		f_out_dataOriginal.close()
+			f_out_dataFull.writelines('%07f'%dataXO[j] + '    ' + '%04f'%dataYO[j]  + '\n')
+		f_out_dataFull.close()
 
 
 

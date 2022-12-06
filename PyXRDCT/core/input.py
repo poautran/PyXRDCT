4b33028b2ed07c9909bf0bc81c46475abfe95dc9
pautran <pierre-olivier.autran@esrf.fr>
2022-12-06 23:28:38 +0100

    Add dataUrls support

--- PyXRDCT/core/input.py
+++ PyXRDCT/core/input.py
@@ -38,31 +38,37 @@ class Input:
         Initialize data path.
         """
         self.data = dataPath
+        self.detector = []
+        if os.path.isfile(self.data):
+            print(self.data)
+        else:
+            print('[ERROR] File not found')       
     
     def getScanGeometry(self):
         """
         Reads scan Geometry from h5 file.
         """
         scanKey=['1']
-        scans = []
-        self.geometry = []
+        scansInput = []
         self.y = []
         self.rot = []
+        self.scans = []
         yMotor = self.yMotor()
         rotMotor = self.rotMotor()
         with h5py.File(self.data,'r') as h5In:
-            self.scans = h5In.keys()
-            for scan in self.scans:
+            scans = list(h5In.keys())
+            for scan in scans:
                 if scan.split('.')[1] == scanKey[0]:
-                    scans.append(int(scan.split('.')[0]))
-        for scan in scans:
-            self.geometry.append('%s.%s'%(scan,scanKey[0]))
+                    scansInput.append(int(scan.split('.')[0]))
+        scansInput = sorted(scansInput)
+        for scan in scansInput:
+            self.scans.append('%s.%s'%(scan,scanKey[0]))
         with h5py.File(self.data,'r') as h5In:
-            for scan in self.geometry:
+            for scan in self.scans:
                 self.y.append(h5In['%s/measurement/%s'%(scan,yMotor)][()])
                 self.rot.append(h5In['%s/measurement/%s'%(scan,rotMotor)][()])
-        print('[INFO] Read %s scans'%max(scans))
-        return self.getScanGeometry
+        print('[INFO] Read %s scans'%max(scansInput))
+    
         
     def yMotor(self):
         """
@@ -73,7 +79,7 @@ class Input:
             for motor in yMotors:
                 if motor in list(h5In['1.1/measurement'].keys()):
                     self.yMotor = motor
-                    print('[INFO] Y motor detected: %s'%self.yMotor)
+        print('[INFO] Y motor detected: %s'%self.yMotor)
         return self.yMotor
         
     def rotMotor(self):
@@ -85,15 +91,41 @@ class Input:
             for motor in rotMotors:
                 if motor in list(h5In['1.1/measurement'].keys()):
                     self.rotMotor = motor
-                    print('[INFO] Rot motor detected: %s'%self.rotMotor)
-        return self.rotMotor          
-                    
-    def readH5(self):
+        print('[INFO] Rot motor detected: %s'%self.rotMotor)        
+        return self.rotMotor
+    
+    def getXrdDetector(self):
         """
-        Reads data from bliss HDF5.
+        Finds XRD detector.
         """
-        if os.path.isfile(self.data):
-            
-            print(self.data)
-        else:
-            print('[ERROR] File not found')
+        xrdDetectors = ['eiger','frelon3']
+        with h5py.File(self.data,'r') as h5In:
+            for detector in xrdDetectors:
+                if detector in list(h5In['1.1/measurement'].keys()):
+                    self.xrddetector = detector
+                    print('[INFO] XRD detector: %s'%self.xrddetector)
+
+    def getXrfDetector(self):
+        """
+        Finds XRF detector.
+        """
+        xrfDetectors = ['mca_det0']
+        with h5py.File(self.data,'r') as h5In:
+            for detector in xrfDetectors:
+                if detector in list(h5In['1.1/measurement'].keys()):
+                    self.xrfdetector = detector
+                    print('[INFO] XRF detector: %s'%self.xrfdetector)  
+
+    def loadData(self):
+        """
+        Loads data Urls from bliss HDF5.
+        """
+        self.getXrfDetector()
+        self.getXrdDetector()
+        self.getScanGeometry()
+        self.dataUrls = []
+        with h5py.File(self.data,'r') as h5In:
+            for scan in self.scans:
+                self.dataUrls.append(h5In.get('%s/measurement/%s'%(scan,self.xrddetector),getlink=True).path)
+        
+        
